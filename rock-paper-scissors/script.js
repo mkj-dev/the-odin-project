2c8121dfcbd9b67e97a40643839b8ebc947497e1
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2022-07-07 21:50:30 +0000

    Add a function for random computer choices

---
+++ rock-paper-scissors/script.js
@@ -0,0 +1,5 @@
+// Generate a random choice from choices array
+function computerPlay() {
+	const choices = ['Rock', 'Paper', 'Scissors'];
+	const choice = choices[Math.floor(Math.random() * choices.length)];
+}
