c19a929201870e14a84376669fa7c35f1201e759
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2022-07-08 01:07:24 +0000

    Add simple prompt for player choice

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,5 +1,17 @@
+const playerSelection = prompt('Type what do you choose - Rock, paper or scissors: ');
+const computerSelection = computerPlay();
+
+console.log(playerSelection);
+console.log(computerSelection);
+
 // Generate a random choice from choices array
 function computerPlay() {
 	const choices = ['Rock', 'Paper', 'Scissors'];
 	const choice = choices[Math.floor(Math.random() * choices.length)];
+	return choice;
+}
+
+// Play one round
+function playRound(playerSelection, computerSelection) {
+	
 }
