131a3b8a92fbcd1215a23a32e60e3a3dfc609d57
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2022-07-08 01:37:45 +0000

    Add a playRound() function

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,8 +1,7 @@
 const playerSelection = prompt('Type what do you choose - Rock, paper or scissors: ');
 const computerSelection = computerPlay();
 
-console.log(playerSelection);
-console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
 
 // Generate a random choice from choices array
 function computerPlay() {
@@ -11,7 +10,21 @@ function computerPlay() {
 	return choice;
 }
 
-// Play one round
+// Play one round with the computer
 function playRound(playerSelection, computerSelection) {
+	// Testing
+	console.log('Player: ', playerSelection);
+	console.log('Computer: ', computerSelection);
 	
+	if (playerSelection === computerSelection) {
+		return 'It\'s a draw!';
+	} else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
+		return 'Computer wins!';
+	} else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
+		return 'Computer wins!';
+	} else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
+		return 'Computer wins!';
+	} else {
+		return 'Player wins!';
+	}
 }
