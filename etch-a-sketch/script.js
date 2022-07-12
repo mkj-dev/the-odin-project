a08a93dba90c294bff607671128db121bc0c62f8
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2022-07-12 13:54:18 +0200

    Add a button for grid size change

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -4,7 +4,7 @@ body.style.textAlign = 'center';
 
 // Main container
 const container = document.querySelector('#container');
-container.setAttribute('style', 'max-width: 960px; border: 1px solid black; margin: 0 auto; padding: 16px')
+container.setAttribute('style', 'max-width: 960px; margin: 0 auto; padding: 16px;')
 
 // Etch-a-sketch square block
 const square = document.createElement('div');
@@ -13,6 +13,12 @@ square.setAttribute('style', 'width: 10px; height: 10px; border: 1px solid black
 // Custom <br> tag
 const br = document.createElement('span');
 br.setAttribute('style', 'display: block; margin-bottom: -0.38em;');
+
+// Button prompt
+const btnPrompt = document.createElement('button');
+btnPrompt.setAttribute('style', 'padding: 4px; border: 1px solid black; border-radius: 8px;')
+btnPrompt.textContent = 'Change grid'
+body.insertBefore(btnPrompt, container);
 
 // Display 16x16 grid of squares
 let i = 0;
@@ -26,7 +32,6 @@ while (i < 16) {
         }
     }
 }
-
 // Draw on mouse over
 for (let n = 0; n < container.childNodes.length; n++) {
     container.childNodes[n].addEventListener('mouseover', () => {
