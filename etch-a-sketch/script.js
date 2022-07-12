d7caaf20c132cabd13d09b7a966182c8bad23c10
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2022-07-12 14:59:02 +0200

    Move some code to functions.js

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -20,38 +20,11 @@ btnPrompt.setAttribute('style', 'padding: 4px; border: 1px solid black; border-r
 btnPrompt.textContent = 'Change grid'
 body.insertBefore(btnPrompt, container);
 
-// Display 16x16 grid of squares
-let i = 0;
-while (i < 16) { 
-    for (let j = 0; j < 16; j++) {
-        container.appendChild(square);
-        container.innerHTML += '';
-        if (j === 15) {
-            container.appendChild(br);
-            i++
-        }
-    }
-}
-
 btnPrompt.addEventListener('click', () => {
     container.textContent = '';
     const userChoice = parseInt(prompt('What size should the grid be? Max. 25.'));
-    let i = 0;
-    while (i < userChoice) { 
-        for (let j = 0; j < userChoice; j++) {
-            container.appendChild(square);
-            container.innerHTML += '';
-            if (j === userChoice - 1) {
-                container.appendChild(br);
-                i++
-            }
-        }
-    }
+    displayGrid(userChoice);
 });
 
-// Draw on mouse over
-for (let n = 0; n < container.childNodes.length; n++) {
-    container.childNodes[n].addEventListener('mouseover', () => {
-        container.childNodes[n].style.backgroundColor = 'black';
-    });    
-}
+// Display 16x16 grid of squares
+displayGrid(16);
