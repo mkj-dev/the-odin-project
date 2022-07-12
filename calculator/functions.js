daa05229ed6c752e89ae9e395c42f6ac09f78976
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2022-07-12 23:44:16 +0200

    Add subtraction function

--- calculator/functions.js
+++ calculator/functions.js
@@ -6,3 +6,11 @@ function add(...args) {
     });
     return result;
 }
+
+function subtract(...args) {
+    let result = args[0];
+    for (let i = 1; i < args.length; i++) {
+        result -= args[i];
+    }
+    return result;
+}
