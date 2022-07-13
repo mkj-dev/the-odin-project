f3fee30f5d0c32896b7f612be8a5564a4f4b565e
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2022-07-13 13:01:39 +0200

    Add constants for operators

--- calculator/script.js
+++ calculator/script.js
@@ -2,5 +2,13 @@ const container = document.getElementById('container');
 const display = document.getElementById('display');
 const buttons = document.querySelectorAll('button');
 
+// Operators
+const addBtn = document.getElementById('add');
+const subtractBtn = document.getElementById('subtract');
+const multiplyBtn = document.getElementById('multiply');
+const divideBtn = document.getElementById('divide');
+// Clear button
+const clearBtn = document.getElementById('clear');
+
 // Make input #display disabled
 display.disabled = true;
