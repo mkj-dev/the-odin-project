3c151b2178b526fef3239bcbdcc13280e9ee959e
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2022-07-13 12:02:48 +0200

    Make #display disabled

--- calculator/script.js
+++ calculator/script.js
@@ -1,3 +1,6 @@
 const container = document.getElementById('container');
 const display = document.getElementById('display');
 const buttons = document.querySelectorAll('button');
+
+// Make input #display disabled
+display.disabled = true;
