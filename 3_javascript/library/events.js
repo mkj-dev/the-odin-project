f5bb057adf9877cd07acb0eafc441b514b423c6f
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2022-07-19 13:41:12 +0200

    Add event for creating new book

--- 3_javascript/library/events.js
+++ 3_javascript/library/events.js
@@ -8,3 +8,10 @@ closeIcon.addEventListener('click', () => {
     formModal.style.display = 'none';
     btnOpenModal.style.display = 'block';
 });
+
+// Add new book object
+btnAddBook.addEventListener('click', () => {
+    let newBook = new Book(bookTitle.value, bookAuthor.value, bookPages.value, isRead.checked);
+    myLibrary.push(newBook);
+    console.log(newBook);
+})
