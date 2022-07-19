678942a4cf9a18c07fb8af6b58f988c51cd05f34
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2022-07-19 12:42:22 +0200

    Add some constants

--- 3_javascript/library/script.js
+++ 3_javascript/library/script.js
@@ -1,5 +1,16 @@
 // Library array
 let myLibrary = [];
+
+// Form modal
+const formModal = document.getElementById('formModal');
+
+// Close icon
+const closeIcon = document.querySelector('.icon span');
+
+// Buttons
+const btnOpenModal = document.getElementById('openModal');
+const btnAddBook = document.getElementById('addBook');
+
 // Book constructor
 function Book(title, author, pages, isRead) {
     this.title = title;
