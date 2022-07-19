4ab736d20bbe3a40f381ece6598b76fd645a6f2e
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2022-07-19 16:17:33 +0200

    Add event for displaying all books in myLibrary array

--- 3_javascript/library/events.js
+++ 3_javascript/library/events.js
@@ -6,12 +6,23 @@ btnOpenModal.addEventListener('click', () => {
 
 closeIcon.addEventListener('click', () => {
     formModal.style.display = 'none';
-    btnOpenModal.style.display = 'block';
+    btnOpenModal.style.display = 'inline-block';
 });
 
 // Add new book object
 btnAddBook.addEventListener('click', () => {
     let newBook = new Book(bookTitle.value, bookAuthor.value, bookPages.value, isRead.checked);
     myLibrary.push(newBook);
-    console.log(newBook);
+    console.log(myLibrary);
 })
+
+// Show library array
+btnShowLibrary.addEventListener('click', () => {
+    myLibrary.forEach(book => {
+        // console.log(book);
+        library.innerHTML += book.title + '<br>'; 
+        library.innerHTML += book.author+ '<br>'; 
+        library.innerHTML += book.pages+ '<br>'; 
+        library.innerHTML += book.isRead+ '<br><hr>'; 
+    });
+});
