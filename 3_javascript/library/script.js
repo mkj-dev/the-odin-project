b707b395d865f0d59a69618a905a7d52725520a5
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2022-07-19 16:14:07 +0200

    Add new consts for book info, display & showLibrary button

--- 3_javascript/library/script.js
+++ 3_javascript/library/script.js
@@ -7,8 +7,24 @@ const formModal = document.getElementById('formModal');
 // Close icon
 const closeIcon = document.querySelector('.icon span');
 
+// Book info
+const bookTitle = document.getElementById('bookTitle');
+const bookAuthor = document.getElementById('bookAuthor');
+const bookPages = document.getElementById('bookPages');
+const isRead = document.getElementById('isRead');
+
+// Book info display
+const library = document.getElementById('library');
+const title = document.createElement('h3');
+const author = document.createElement('h4');
+const pages = document.createElement('p');
+const read = document.createElement('p');
+const hr = document.createElement('hr');
+
+
 // Buttons
 const btnOpenModal = document.getElementById('openModal');
+const btnShowLibrary = document.getElementById('showLibrary');
 const btnAddBook = document.getElementById('addBook');
 
 // Book constructor
