f30649e9c9120e8388ea5e4763376f74c18535e1
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2022-08-06 20:33:18 +0200

    Add draw mechanics

--- 3_javascript/tic-tac-toe/script.js
+++ 3_javascript/tic-tac-toe/script.js
@@ -24,7 +24,7 @@ document.addEventListener('click', event => {
     const target = event.target;
     const isCell = target.classList.contains('block');
     const isDisabled = target.classList.contains('disabled');
-    
+
     if (isCell && !isDisabled) {
         // The player clicked on a cell that is still empty
         const cellValue = target.dataset.value;
@@ -39,5 +39,11 @@ document.addEventListener('click', event => {
         target.classList.add('disabled');
         target.classList.add(game.xTurn ? 'x' : 'o');
         game.xTurn = !game.xTurn;
+    }
+
+    if (document.querySelectorAll('body > div > div.block.disabled').length === 9) {
+        const h3 = document.createElement('h3');
+        h3.innerText = 'Draw!';
+        document.querySelector('h2').appendChild(h3);
     }
 });
