43a19df319e95c244dafff1d8ebfbacbecf7cfa1
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2022-08-06 20:01:23 +0200

    Add X & O display on the game board

--- 3_javascript/tic-tac-toe/script.js
+++ 3_javascript/tic-tac-toe/script.js
@@ -19,3 +19,25 @@ const game = {
         ['2', '4', '6']
     ]
 }
+
+document.addEventListener('click', event => {
+    const target = event.target;
+    const isCell = target.classList.contains('block');
+    const isDisabled = target.classList.contains('disabled');
+    
+    if (isCell && !isDisabled) {
+        // The player clicked on a cell that is still empty
+        const cellValue = target.dataset.value;
+
+        if (game.xTurn === true)
+            game.xState.push(cellValue);
+        else
+            game.oState.push(cellValue);
+        // console.log('X:', game.xState);
+        // console.log('O', game.oState);
+
+        target.classList.add('disabled');
+        target.classList.add(game.xTurn ? 'x' : 'o');
+        game.xTurn = !game.xTurn;
+    }
+});
